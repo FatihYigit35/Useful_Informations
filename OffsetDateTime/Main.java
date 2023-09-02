c4f8f807c9fa544e1acb963b283330ead92abd1f
Fatih YİĞİT <65901286+FatihYigit35@users.noreply.github.com>
2023-09-02 17:31:31 +0300

    Update Main.java

--- OffsetDateTime/Main.java
+++ OffsetDateTime/Main.java
@@ -56,5 +56,37 @@ public class Main {
         System.out.println(OffsetDateTimeParse);
         System.out.println("----------------------------");
 
+        /*
+            -> My Output:
+            ----------------------------
+            localDateTimeNow
+            2023-09-02T17:27:46.897558200
+            ----------------------------
+            localDateTimeParse
+            2023-02-08T02:30:55
+            ----------------------------
+            offsetDateTimeFromLocalDateTimeParse + ZoneOffset.ofHours(-3)
+            2023-02-08T02:30:55-03:00
+            ----------------------------
+            offsetDateTimeFromWithOffsetSameInstant + ZoneOffset.ofHours(+2)
+            2023-02-08T07:30:55+02:00
+            ----------------------------
+            offsetDateTimeNowWithZoneOffSetOfHoursMinutes + ZoneOffset.ofHoursMinutes(-3, -30)
+            2023-09-02T10:57:46.903545-03:30
+            ----------------------------
+            offsetDateTimeWithZoneOffSetOf + ZoneOffset.of("+03:00")
+            2023-09-02T17:27:46.903545+03:00
+            ----------------------------
+            offsetDateTimeWithZoneOffSetOf.format(isoFormatter)
+            2023-09-02T17:27:46.903545+03:00
+            ----------------------------
+            offsetDateTimeWithZoneOffSetOf.format(myFormatter)
+            2023-09-02T17:27:46+03:00
+            ----------------------------
+            OffsetDateTimeParse
+            2023-02-08T02:30:30+03:00
+            ----------------------------
+        */
+
     }
 }
